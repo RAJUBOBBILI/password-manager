99871f352c4f98e0fed3cb290fc8bd972de0d77f
raju <rajubobbili345@gmail.com>
2023-05-03 18:39:58 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import PasswordManager from './component/PasswordManager'
+import './App.css'
+
+const App = () => (
+  <div>
+    <PasswordManager />
+  </div>
+)
+
+export default App
---
+++ src/component/InputSection/index.js
@@ -0,0 +1,42 @@
+import './index.css'
+
+const InputItem = props => {
+  const {itemDetails, isChecked, onDeleteItem} = props
+  const {id, websiteInput, passwordInput, username} = itemDetails
+
+  const passwordItem = isChecked ? (
+    <p className="para">{passwordInput}</p>
+  ) : (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+      alt="stars"
+    />
+  )
+  const initial = websiteInput[0].toUpperCase()
+
+  const onClickDelet = () => {
+    onDeleteItem(id)
+  }
+
+  return (
+    <li className="list-item-container">
+      <div className="initial-section">{initial}</div>
+      <div className="text-cont">
+        <p className="heading">{websiteInput}</p>
+        <p className="para">{username}</p>
+        {passwordItem}
+      </div>
+      <div className="button-cont">
+        <button type="button" className="delete-button" onClick={onClickDelet}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default InputItem
